e792afee7ac2681b546320f104bfddfded5fca4f
Ocassio <orlicus@gmail.com>
2015-03-31 21:16:57 +0400

    Webix, AngularJS and REST controller integration

--- src/main/webapp/js/controllers/catalogController.js
+++ src/main/webapp/js/controllers/catalogController.js
@@ -1,3 +1,10 @@
-portal.controller("CatalogController", function() {
+portal.controller("CatalogController", function($http) {
+
+    var portal = this;
+    this.offers = [];
+
+    $http.get("/online-store/rest/offers/getAllOffers").success(function(data) {
+        portal.offers = data;
+    });
 
 });
