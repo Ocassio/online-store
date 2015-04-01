c47a3d18edb9fb1d44bac31f3cfbb46b3e695ea8
Ocassio <orlicus@gmail.com>
2015-04-01 17:12:12 +0400

    Offers page design (work in progress)

--- src/main/java/ru/bpr/onlinestore/portal/services/catalog/CatalogServiceStub.java
+++ src/main/java/ru/bpr/onlinestore/portal/services/catalog/CatalogServiceStub.java
@@ -14,6 +14,7 @@ public class CatalogServiceStub implements CatalogService
         List<OfferViewModel> offers = new ArrayList<OfferViewModel>();
         offers.add(new OfferViewModel("1", "Offer 1", "Offer 1 description", "$1000"));
         offers.add(new OfferViewModel("2", "Offer 2", "Offer 2 description", "$2000"));
+        offers.add(new OfferViewModel("3", "Offer 3", "Offer 3 description", "$3000"));
 
         return offers;
     }
