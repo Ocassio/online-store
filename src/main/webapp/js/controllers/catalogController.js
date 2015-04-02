1498e3e72b2721a83fb8c7d4cbfb0b468b69ce84
Ocassio <orlicus@gmail.com>
2015-04-02 21:46:21 +0400

    Deleted Webix page and library, added Angular UI Bootstrap, pagination implementation

--- src/main/webapp/js/controllers/catalogController.js
+++ src/main/webapp/js/controllers/catalogController.js
@@ -1,10 +1,25 @@
 portal.controller("CatalogController", function($http) {
 
     var portal = this;
+
+    this.totalItems = 0;
+    this.itemsPerPage = 6;
+    this.currentPage = 1;
+    this.maxSize = 5;
+
+    this.setPage = function(page) {
+        this.currentPage = page;
+    };
+
+    this.pageChanged = function() {
+
+    };
+
     this.offers = [];
 
     $http.get("/online-store/rest/offers/getAllOffers").success(function(data) {
         portal.offers = data;
+        portal.totalItems = data.length;
     });
 
 });
--- src/main/webapp/js/modules.js
+++ src/main/webapp/js/modules.js
@@ -1,1 +1,8 @@
-var portal = angular.module("portal", []);
+var portal = angular.module("portal", ["ui.bootstrap"]);
+
+portal.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
