61e35502f365f85e6d82a74f86ed1c6e61d8b7ff
vago <goncharov.valia@yandex.ru>
2015-04-05 19:26:00 +0400

    add Hibernate

---
+++ src/main/java/ru/bpr/onlinestore/portal/offers/models/Offer.java
@@ -0,0 +1,24 @@
+package ru.bpr.onlinestore.portal.offers.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "offers")
+public class Offer implements Serializable
+{
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category")
+    private Category category;
+}
--- src/main/java/ru/bpr/onlinestore/portal/services/catalog/CatalogServiceImpl.java
+++ src/main/java/ru/bpr/onlinestore/portal/services/catalog/CatalogServiceImpl.java
@@ -1,6 +1,12 @@
 package ru.bpr.onlinestore.portal.services.catalog;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.bpr.onlinestore.portal.offers.models.Category;
 import ru.bpr.onlinestore.portal.offers.models.CategoryViewModel;
 import ru.bpr.onlinestore.portal.offers.models.OfferViewModel;
 
@@ -8,10 +14,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class CatalogServiceImpl implements CatalogService
 {
+    @Autowired
+    private SessionFactory sessionFactory;
+
     public List<OfferViewModel> getOffers()
     {
+        categories();
         List<OfferViewModel> offers = new ArrayList<OfferViewModel>();
         offers.add(new OfferViewModel("3", "Offer 1", "1", "Offer 1 description", "$1000", "4"));
         offers.add(new OfferViewModel("2", "Offer 2", "2", "Offer 2 description", "$2000", "3"));
@@ -22,5 +33,12 @@ public class CatalogServiceImpl implements CatalogService
     public List<CategoryViewModel> getCategories()
     {
         return new ArrayList<CategoryViewModel>();
+    }
+
+    private List<Category> categories()
+    {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("FROM Category");
+        return query.list();
     }
 }
