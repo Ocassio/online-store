90025ac779e0ff2a8b8b2cb85b5cec720e169454
VAGO <goncharov.valia@yandex.ru>
2015-04-09 22:59:18 +0400

    fix spring DI for hibernate

--- src/main/java/ru/bpr/onlinestore/portal/services/catalog/CatalogServiceImpl.java
+++ src/main/java/ru/bpr/onlinestore/portal/services/catalog/CatalogServiceImpl.java
@@ -1,22 +1,28 @@
 package ru.bpr.onlinestore.portal.services.catalog;
 
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.bpr.onlinestore.portal.models.catalog.CategoryViewModel;
 import ru.bpr.onlinestore.portal.models.catalog.OfferViewModel;
+import ru.bpr.onlinestore.portal.services.models.Category;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-//@Transactional
+@Transactional
 public class CatalogServiceImpl implements CatalogService
 {
-    /*@Resource(name = "sessionFactory")
-    private SessionFactory sessionFactory;*/
+    @Autowired(required = false)
+    private SessionFactory sessionFactory;
     List<OfferViewModel> offers;
     public List<OfferViewModel> getOffers()
     {
-        //categories();
+        categories();
         offers = new ArrayList<OfferViewModel>();
         offers.add(new OfferViewModel("3", "Offer 1", "1", "Offer 1 description", "$1000", "4"));
         offers.add(new OfferViewModel("2", "Offer 2", "2", "Offer 2 description", "$2000", "3"));
@@ -34,10 +40,10 @@ public class CatalogServiceImpl implements CatalogService
         return offers.get(Integer.parseInt(offerId));
     }
 
-    /*private List<Category> categories()
+    private List<Category> categories()
     {
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("FROM Category");
         return query.list();
-    }*/
+    }
 }
