10680bd702017627223d318e41e915d0cce913a0
Ocassio <orlicus@gmail.com>
2015-04-11 14:28:42 +0400

    Cart - Selection and offers removal

--- src/main/webapp/js/portal/controllers/cart-controller.js
+++ src/main/webapp/js/portal/controllers/cart-controller.js
@@ -1,16 +1,38 @@
 portalControllers.controller("CartController", function($modal, shoppingCart) {
 
+    this.selectedOffer = null;
+
     this.clear = function() {
         var dialog = $modal.open({
             templateUrl: "views/portal-cart-clear-confirm.html",
             controller: "CartClearConfirmController"
         });
 
-        dialog.result.then(this.onConfirm);
+        dialog.result.then(this.onConfirm.bind(this));
     };
 
     this.onConfirm = function() {
+        this.selectedOffer = null;
         shoppingCart.clear();
+    };
+
+    this.isSelected = function(offer) {
+        return offer === this.selectedOffer;
+    };
+
+    this.select = function(offer) {
+        if (!this.isSelected(offer)) {
+            this.selectedOffer = offer;
+        } else {
+            this.selectedOffer = null;
+        }
+    };
+
+    this.removeSelected = function() {
+        if (this.selectedOffer) {
+            shoppingCart.remove(this.selectedOffer);
+            this.selectedOffer = null;
+        }
     };
 
 });
